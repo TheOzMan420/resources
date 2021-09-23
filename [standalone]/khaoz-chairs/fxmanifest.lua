7f8953a3e8dc2b1381a57ddc5c11d53f1a178c52
criticalrarex <criticalrarex@gmail.com>
2021-09-23 20:37:00 +0200

    Initial commit

---
+++ [standalone]/khaoz-chairs/fxmanifest.lua
@@ -0,0 +1,14 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'lawnchairs'
+version '1.0.0'
+
+client_script 'client/main.lua'
+server_script 'server/main.lua'
+
+shared_scripts { 
+	'@qb-core/import.lua',
+	'config.lua'
+}
+
---
+++ [standalone]/khaoz-chairs/server/main.lua
@@ -0,0 +1,5 @@
+-- Leaked By: Leaking Hub | J. Snow | leakinghub.com
+QBCore.Functions.CreateUseableItem("lawnchair", function(source, item)
+    local Player = QBCore.Functions.GetPlayer(source)
+    TriggerClientEvent('lawnchairs:client:chair1', source)
+end)
